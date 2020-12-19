ed673300a720570c12234ea79c63dd1a5d35d003
geoffroymounier <geoffroymounier@gmail.com>
2020-12-19 17:38:11 +0100

    again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   darkMode: 'media', // 'media' or 'class',
   purge: [
-    '../_next/**/*.{js,ts,jsx,tsx,html}',
-    './_next/**/*.{js,ts,jsx,tsx,html}',
-    './dist/_next/**/*.{js,ts,jsx,tsx,html}',
-    '_next/**/*.{js,ts,jsx,tsx,html}'],
+    '../_next/**/*.{js,ts,jsx,tsx,html,json}',
+    './_next/**/*.{js,ts,jsx,tsx,html,json}',
+    './dist/_next/**/*.{js,ts,jsx,tsx,html,json}',
+    '_next/**/*.{js,ts,jsx,tsx,html,json}',
+    '../.next/**/*.{js,ts,jsx,tsx,html,json}',
+    './.next/**/*.{js,ts,jsx,tsx,html,json}',
+    './dist/.next/**/*.{js,ts,jsx,tsx,html,json}',
+    '.next/**/*.{js,ts,jsx,tsx,html,json}'
+  ],
   theme: {
     extend: {
       colors: {
