5b24e1516e890f0022c4cdff0112ad9c309c9d64
geoffroymounier <geoffroymounier@gmail.com>
2020-12-20 11:45:37 +0100

    add script

---
+++ src/utils/Purge.js
@@ -0,0 +1,35 @@
+
+ const fs = require('fs');
+ 
+ const migration = async (migration, context) => {
+  
+  const makeRequest = context.makeRequest
+  const {items} = await makeRequest({
+    method: 'GET',
+    url: `/entries`
+  });
+
+  const classArray = [...new Set(items.reduce((acc,item) => {
+    if (item.fields.classes) {
+      const classByLang = Object.keys(item.fields.classes).reduce((acc,key) => {
+        const array = item.fields.classes[key].split(' ')
+        return [...acc,...array]
+      },[])
+
+      return [...new Set([...acc,...classByLang])]
+    }
+    return acc
+  },[]))]
+  const string = JSON.stringify(classArray)
+
+  fs.writeFile('./classArray.json', string, function (err,data) {
+    if (err) {
+      return console.log({err});
+    }
+    console.log('migrated');
+  });
+  return
+
+};
+
+module.exports = migration;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   darkMode: 'media', // 'media' or 'class',
-  purge: [
-    '../_next/**/*.{js,ts,jsx,tsx,html,json}',
-    './_next/**/*.{js,ts,jsx,tsx,html,json}',
-    './dist/_next/**/*.{js,ts,jsx,tsx,html,json}',
-    '_next/**/*.{js,ts,jsx,tsx,html,json}',
-    '../.next/**/*.{js,ts,jsx,tsx,html,json}',
-    './.next/**/*.{js,ts,jsx,tsx,html,json}',
-    './dist/.next/**/*.{js,ts,jsx,tsx,html,json}',
-    '.next/**/*.{js,ts,jsx,tsx,html,json}'
-  ],
+  purge: {
+    content: ['./classArray.json'],
+    css: ['./styles/index.css'],
+  },
   theme: {
     extend: {
       colors: {
