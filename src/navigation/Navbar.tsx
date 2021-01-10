22aaeb010848ba6ff4199a7108254c1757e74c19
geoffroymounier <geoffroymounier@gmail.com>
2021-01-10 17:51:55 +0100

    feat: [CP-361] add responsive header

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import dynamic from 'next/dynamic'
 import classnames from 'classnames';
+import {CSSTransition} from 'react-transition-group'
+import { useWidth } from '../utils/env'
 import styled from 'styled-components'
 
 const MediaEnriched = dynamic(() => import('../content/MediaEnriched'));
@@ -10,23 +12,39 @@ enum Items {
   BUTTON = 'buttonField',
 }
 
+interface MenuItemProps {
+  items: any,
+  name: string,
+  type: string,
+  classes: string,
+  style: string,
+  link: any,
+  itemClasses: string,
+  itemStyle: string,
+  interactionIn: string[]
+  subItemType?:string;
+  changeOnHover?: boolean;
+}
+
 const validItems = {
   [Items.MEDIA_ENRICHED]: MediaEnriched,
   [Items.BUTTON]: Button,
 };
 const WrappedDiv = styled.div`${props => props.styled}`;
 const WrappedUl = styled.ul`${props => props.styled}`;
+const WrappedLi = styled.li`${props => props.styled}`;
+
 const HeaderWrapper = styled.header`${props => props.styled}`;
 const MenuContext = React.createContext({
   openMenu: '',
   changeOpenMenu: (e) => { return e },
 })
 
-function MenuItem(props) {
+const MenuItem: React.FC<MenuItemProps> = React.memo((props) => {
   const buttonRef = React.useRef(null)
+  
   const { openMenu, changeOpenMenu } = React.useContext(MenuContext)
-  const { items, classes, style, link, itemClasses, itemStyle } = props
-
+  const { items, classes, style, link, itemClasses, itemStyle, interactionIn = [] } = props
   if (items?.length) {
     return (
       <WrappedDiv className={classes} styled={style}>
@@ -34,21 +52,25 @@ function MenuItem(props) {
         {link ?
           <li
             ref={buttonRef}
-            onMouseEnter={() => changeOpenMenu(props.name)}
+            onMouseEnter={() =>interactionIn.findIndex(val => val === 'hover') > -1 && changeOpenMenu(props.name)}
+            onClick={() => interactionIn.findIndex(val => val === 'click') > -1 && changeOpenMenu(props.name)}
           >
             <Button {...props.link} />
-            {openMenu === props.name && <WrappedUl
-              onMouseEnter={() => changeOpenMenu(props.name)}
+            {<CSSTransition in={openMenu === props.name} timeout={200} classNames="transition">
+              <WrappedUl
               className={classnames('absolute z-10', itemClasses)}
               styled={itemStyle}
             >
               {items.map((item, idx) => {
-                return <MenuItem key={idx} {...item} />
+                return <MenuItem key={idx} {...item}  />
               })}
-            </WrappedUl>}
+            </WrappedUl>
+            </CSSTransition>
+            }
           </li> :
           <WrappedUl
-            onMouseEnter={() => changeOpenMenu(props.name)}
+          onMouseEnter={() =>interactionIn.findIndex(val => val === 'hover') > -1 && changeOpenMenu(props.name)}
+            onClick={() => interactionIn.findIndex(val => val === 'click') > -1 && changeOpenMenu(props.name)}
             className={classnames('', itemClasses)}
             styled={itemStyle}
           >
@@ -57,33 +79,48 @@ function MenuItem(props) {
             })}
           </WrappedUl>
         }
-
-
       </WrappedDiv>
     )
   } else if (validItems[props.type]) {
     const Component = validItems[props.type]
-    
-    return (<li className={classes} style={style}><Component {...props} /></li>)
+
+    return (<WrappedLi className={classes} styled={style}><Component {...props} /></WrappedLi>)
   } else return null
 
-}
+})
 
 const Navbar = (props: any, ref) => {
   const [openMenu, setOpenMenu] = React.useState<string>('')
+  const top = ref.current?.offsetTop
+  const [width] = useWidth()
+  const menu = React.useMemo(() => width > 700 ? props.desktop : props.mobile ,[width])
   const changeOpenMenu = (e: string) => setOpenMenu(e)
-
+  
   return (
-    <HeaderWrapper
-      ref={ref}
+    <>
+      <div
+        onClick={() => changeOpenMenu('')}
+        onMouseEnter={() => changeOpenMenu('')}
+        className={'transition-all inset-x-0 fixed bg-gray-500'}
+        style={{
+          top,
+          zIndex: openMenu ? 10 : 0,
+          bottom: openMenu ? '0%' : '100%',
+          opacity: openMenu ? '0.5' : '0',
+        }}>
+      </div>
+      <HeaderWrapper
+        ref={ref}
 
-      onMouseLeave={() => changeOpenMenu('')}
-      sticky={props.sticky}
-      className={classnames(`${props.sticky ? 'fixed' : 'relative'} ${props.classes}`)} styled={props.styled}>
-      <MenuContext.Provider value={{ openMenu, changeOpenMenu }}>
-        <MenuItem {...props.desktop} />
-      </MenuContext.Provider>
-    </HeaderWrapper>
+        sticky={props.sticky}
+        className={classnames(`${props.sticky ? 'fixed' : 'relative'} ${props.classes}`)} styled={props.styled}>
+
+        <MenuContext.Provider value={{ openMenu, changeOpenMenu }}>
+          <MenuItem {...menu} />
+        </MenuContext.Provider>
+      </HeaderWrapper>
+    </>
+
   )
 };
 
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,30 @@
+import React from 'react';
 const isBrowser = () => !!process.browser
 const isDev = process.env.NODE_ENV === 'development'
-const getWidth = () => {
-  return isBrowser() ? window.innerWidth : 0
+
+
+const useWidth = (elementRef?) => {
+    const [width, setWidth] = React.useState(null);
+
+    const updateWidth = React.useCallback(() => {
+        if(elementRef) {
+            const { width } = elementRef.getBoundingClientRect();
+            setWidth(width);
+        } else {
+          const { width : windowWidth } = document.body.getBoundingClientRect();
+          setWidth(windowWidth)
+        }
+    }, [elementRef]);
+
+    React.useEffect(() => {
+        updateWidth();
+        window.addEventListener('resize', updateWidth);
+        return () => {
+            window.removeEventListener('resize', updateWidth);
+        }
+    }, [updateWidth])
+
+    return [width]
 }
-export {getWidth,isBrowser,isDev}
+
+export {useWidth,isBrowser,isDev}
